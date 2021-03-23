2a5bf3ad4359d91495cb4022629276af66e3466b
yujian <yujian@ysten.com>
2021-03-23 22:39:57 +0800

    Initial commit

---
+++ src/main/java/com/hogwarts/ushio/UshioDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.ushio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshioDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UshioDemoApplication.class, args);
+	}
+
+}
